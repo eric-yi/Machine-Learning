d8918d0ac60b779d17122ac14c540c6d4f9bb430
Xiaobin Yi <xbyi@thoughtworks.com>
2016-10-08 23:33:11 +0800

    chore(bosch-production-line-performance-160817): truncate data list

--- kaggle/bosch-production-line-performance-160817/src/app.py
+++ kaggle/bosch-production-line-performance-160817/src/app.py
@@ -2,14 +2,15 @@
 # -*- coding:utf-8 -*-
 
 import csv
+import random
 
-def load_csv(csv_file, rows):
+def load_csv(csv_file, rows=-1):
     datalist = []
     with open(csv_file, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         n = 0
         for row in reader:
-            if n >= rows: break
+            if rows != -1 and n >= rows: break
             datalist.append(row[0])
             n += 1
     return datalist
@@ -18,6 +19,30 @@ def load_csv(csv_file, rows):
 TRAIN_CATEGORICAL = '../logs/train_categorical.csv'
 def load_labels(csv_file=TRAIN_CATEGORICAL):
     return load_csv(csv_file, 1)[0]
+
+def truncate(csv_files, dst_files, factor=10):
+    datalist_group = []
+    for csv_file in csv_files:
+        datalist = load_csv(csv_file)
+        datalist_group.append(datalist)
+    truncate_file_group = []
+    for dst_file in dst_files:
+        truncate_file = open(dst_file, 'w')
+        truncate_file_group.append(truncate_file)
+    n = 0
+    size = len(datalist)
+    while True:
+        start = n * factor
+        index = start + int(random.random() * factor)
+        if index > size:    break
+        m = 0
+        while m < len(datalist_group):
+            datalist = datalist_group[m]
+            truncate_file = truncate_file_group[m]
+            truncate_file.write(datalist[index])
+            truncate_file.write('\n')
+            m += 1
+        n += 1
 
 def run():
     print 'start run...'
--- kaggle/bosch-production-line-performance-160817/test/test_app.py
+++ kaggle/bosch-production-line-performance-160817/test/test_app.py
@@ -7,18 +7,32 @@ sys.path.append('../src')
 import app
 
 class TestCase(unittest.TestCase):
+    @unittest.skip('skip')
     def test_run(self):
         app.run()
         self.assertTrue(True)
 
+    @unittest.skip('skip')
     def test_load_csv(self):
         csv_file = '../logs/train_categorical.csv'
-        app.load_csv(csv_file, 10)
+        app.load_csv(csv_file, -1)
         self.assertTrue(True)
 
+    @unittest.skip('skip')
     def test_load_labels(self):
         data_lables = app.load_labels()
         print data_lables
         self.assertTrue(len(data_lables) > 0)
+
+    def test_truncate(self):
+        factor = 100
+        csv_files = ['../logs/train_categorical.csv',
+                     '../logs/train_date.csv',
+                     '../logs/train_numeric.csv']
+        dst_files = ['../logs/train_categorical_%d.csv' % (factor),
+                     '../logs/train_date_%d.csv' % (factor),
+                     '../logs/train_numeric_%d.csv' % (factor)]
+        app.truncate(csv_files, dst_files, factor)
+        self.assertTrue(True)
 
 unittest.main()
